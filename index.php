1bc6f79b074fede138c585055a2f5d22ced81bc7
atelierabbey <info@sillycoyote.com>
2014-06-30 18:56:12 -0700

    Added back the missing index file... added date

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	Plugin Name: WP Jcycle2
+	Plugin URI: https://github.com/atelierabbey/WPJcycle
+	Description: A simple slider plugin - based on Malsup's Cycle2 http://jquery.malsup.com/cycle2/.
+	Version: 27Jun14
+	Author: Grayson A.C. Laramore
+	Author URI: http://wwww.SillyCoyote.com
+	License: GPL2
+*/
+include ('inc/lib/skivvy_wpjcycle.php');
+
+
+?>
