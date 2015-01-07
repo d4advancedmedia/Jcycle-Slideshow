cd4d49088bea550cabc3d9de40aea1d77940a0aa
atelierabbey <info@sillycoyote.com>
2015-01-07 15:30:56 -0800

    Remove skinfo() reliance

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	Plugin Name: Skivvy Slideshow
 	Plugin URI: https://github.com/atelierabbey/WPJcycle
 	Description: A simple slider plugin - based on Malsup's Cycle2 http://jquery.malsup.com/cycle2/.
-	Version: 19Sep14
+	Version: 7Jan15
 	Author: Grayson A.C. Laramore
 	License: GPL2
 */
@@ -13,7 +13,7 @@ include ('inc/lib/metabox-video-attachment.php');
 function skivvy_admin_slideshow_style() {
 
 	// Styles
-	wp_register_style( 'skivvy_admin_slideshow_style', plugins_url( '/inc/admin/admin.css' , __FILE__ ), false, skinfo('Version'), 'screen' );
+	wp_register_style( 'skivvy_admin_slideshow_style', plugins_url( '/inc/admin/admin.css' , __FILE__ ), false, 1, 'screen' );
 	wp_enqueue_style( 'skivvy_admin_slideshow_style' );
 
 
