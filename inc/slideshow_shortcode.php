47b5fef58faeb1b7e181f93aa4464680b05d2b6e
GrayofD4 <GrayofD4@users.noreply.github.com>
2015-09-01 12:08:05 -0700

    Added optional attr - 'slides'

--- inc/slideshow_shortcode.php
+++ inc/slideshow_shortcode.php
@@ -1,4 +1,4 @@
-<?php #17Aug15a
+<?php #01Sep15a
 
 // Register shortcode
 	add_shortcode('slideshow','register_d4slideshow_slideshow');
@@ -6,10 +6,24 @@
 	function register_d4slideshow_slideshow( $atts, $content = null ) {
 			wp_enqueue_script('jcycle2');
 			$attr = shortcode_atts( array(
-				'time' => '4000'
+				'time'         => '4000',
+				'pager'        => 'false',
+				'slides'       => '.cycle-slide'
 			), $atts );
-			$output = '<div class="cycle-slideshow" style="position:relative;" data-cycle-slides=".cycle-slide" data-timeout="' . $attr['time'] . '" >';
+			$output = '<div';
+				$output .= ' class="cycle-slideshow"';
+				$output .= ' style="position:relative;"';
+				$output .= ' data-cycle-slides="' . $attr['slides'] . '"';
+				$output .= ' data-timeout="' . $attr['time'] . '"';
+				if ( $attr['pager'] != 'false' ) {
+					$output .= ' data-cycle-pager="#per-slide-template"';
+				}
+			$output .= '>';
 				$output .= do_shortcode($content);
+
+				if ( $attr['pager'] != 'false' ) {
+					$output .= '<div id="per-slide-template"></div>';
+				}
 			$output .= '</div>';
 			return $output;
 
